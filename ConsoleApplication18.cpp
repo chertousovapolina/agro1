68a576d915d80cbe6aeea36f46745d730d1d6ad3
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-12-03 22:40:03 +0300

    Add files via upload

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication18.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+struct agro{
+    string name;
+    int klass;
+    float oz;
+    int prop;
+    int sila;
+};
+int main()
+{
+    agro agro1;
+    agro1.name = "Ebik";
+    agro1.klass = 6;
+    agro1.oz = 3.2;
+    agro1.prop = 10;
+    agro1.sila = 5;
+
+      
+}
+
+
